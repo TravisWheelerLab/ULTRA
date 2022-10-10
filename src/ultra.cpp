a77287f0c92e9aacb74b79708126495588cdbfca
danielolson <dulcicannon@gmail.com>
2022-10-10 12:43:58 -0600

    Reformatted repeats_overlap(...)

--- src/ultra.cpp
+++ src/ultra.cpp
@@ -402,11 +402,13 @@ bool CompareRepeatOrder::operator()(RepeatRegion *lhs, RepeatRegion *rhs) {
 
 bool repeats_overlap(RepeatRegion *r1, RepeatRegion *r2,
                      bool require_same_period) {
-  if (r1->sequenceID == r2->sequenceID)
-    if (require_same_period)
-      if (r1->repeatPeriod == r2->repeatPeriod)
+  if (r1->sequenceID == r2->sequenceID) {
+    if (require_same_period) {
+      if (r1->repeatPeriod == r2->repeatPeriod) {
         return (r1->sequenceStart + r1->repeatLength >= r2->sequenceStart);
-      else
-        return (r1->sequenceStart + r1->repeatLength >= r2->sequenceStart);
+      }
+      return (r1->sequenceStart + r1->repeatLength >= r2->sequenceStart);
+    }
+  }
   return false;
 }
