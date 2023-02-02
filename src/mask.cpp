6a96a39a16a100e34a50b193b530f70e7a2fa363
danielolson <dulcicannon@gmail.com>
2023-02-02 13:06:04 -0700

    Mor masking code

--- src/mask.cpp
+++ src/mask.cpp
@@ -6,8 +6,8 @@
 
 void OutputMaskedFASTA(std::string in_path,
                        FILE* out_file,
-                       const std::unordered_map<std::string,
-                                                std::vector<mregion> *> &masks) {
+                        std::unordered_map<std::string,
+                                                std::vector<mregion> *> masks) {
 
   std::ifstream in_file(in_path);
   if (!in_file.is_open()) {
@@ -17,11 +17,63 @@ void OutputMaskedFASTA(std::string in_path,
 
   std::string line;
 
-  std::string sequence_name
+  std::string seq_name = "";
+
+  unsigned long long seq_pos = 0;
+
+  std::vector <mregion> *cmask = nullptr;
 
   while(std::getline(in_file, line)) {
+
+    if (line[0] == '>') {
+      seq_name = line.substr(1, std::string::npos);
+      fprintf(out_file, "%s", line.c_str());
+
+      if (masks.find(seq_name) != masks.end()) {
+        delete cmask;
+        cmask = CleanedMasks(masks[seq_name]);
+      }
+    }
+
+    else {
+      if (cmask == nullptr) {
+        fprintf(out_file, "%s", line.c_str());
+        continue;
+      }
+    }
 
   }
 
   in_file.close();
+}
+
+bool mregion::operator()(mregion m1, mregion m2) {
+  if (m1.start != m2.start)
+    return (m1.start > m2.start);
+
+  else
+    return m1.end > m2.end;
+}
+
+std::vector<mregion> *CleanedMasks(std::vector<mregion> *mask) {
+  if (mask->empty())
+    return nullptr;
+
+  std::sort(mask->begin(), mask->end(), mregion());
+
+  // combine overlapped regions
+  std::vector<mregion> *cmask = new std::vector<mregion>();
+  cmask->push_back(mask->at(0));
+  for (int i = 1; i < mask->size(); ++i) {
+    if (mask->at(i).start <= cmask->back().end) {
+      if (mask->at(i).end > cmask->back().end)
+        cmask->back().end = mask->at(i).end;
+    }
+
+    else {
+      cmask->push_back(mask->at(i));
+    }
+  }
+
+  return cmask;
 }
--- src/mask.h
+++ src/mask.h
@@ -10,14 +10,28 @@
 #include <unordered_map>
 #include <sstream>
 #include <fstream>
+
 typedef struct s_mask_region {
   unsigned long long start;
   unsigned long long end;
+
+  bool operator()(s_mask_region m1, s_mask_region m2);
 } mregion;
 
-void OutputMaskedFASTA(std::string in_path,
+void OutputCaseMaskedFASTA(std::string in_path,
                        FILE* out_file,
-                       const std::unordered_map<std::string,
-                                                std::vector<mregion> *> &masks);
+                        std::unordered_map<std::string,
+                                                std::vector<mregion> *> masks);
+void OutputNMaskedFASTA(std::string in_path,
+                           FILE* out_file,
+                            std::unordered_map<std::string,
+                                                    std::vector<mregion> *> masks);
+void OutputRemoveMaskedFASTA(std::string in_path,
+                           FILE* out_file,
+                            std::unordered_map<std::string,
+                                                    std::vector<mregion> *> masks);
+std::vector<mregion> *CleanedMasks(std::vector<mregion> *mask);
+
+
 
 #endif // ULTRA_MASK_H
