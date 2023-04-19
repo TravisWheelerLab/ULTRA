e50c022acb959aae55ea130e915d9aa53fecfd38
Daniel Olson <daniel.olson@me.com>
2023-04-19 15:43:44 -0600

    Updating version number

--- src/cli.hpp
+++ src/cli.hpp
@@ -4,7 +4,7 @@
 
 #ifndef ULTRA_CLI_HPP
 #define ULTRA_CLI_HPP
-#define ULTRA_VERSION_STRING "1.0.0 (beta 0)"
+#define ULTRA_VERSION_STRING "1.0.0 (beta 1)"
 #include "../lib/CLI11.hpp"
 #include <string>
 #include <vector>
