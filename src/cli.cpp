9e05d094687cf27c826ab4b63c7ae8fe95322587
Daniel Olson <daniel.olson@me.com>
2025-09-02 07:54:56 -0600

    --tune_fdr now lists the default value in the help

--- src/cli.cpp
+++ src/cli.cpp
@@ -207,6 +207,7 @@ void Settings::prepare_settings() {
 
   app.add_option("--tune_fdr", this->tune_fdr,
                  "FDR to be tuned against (see README)")
+      ->default_val(this->tune_fdr)
       ->group("Parameter Tuning");
 
   app.add_flag("--tune_only", this->tune_only,
